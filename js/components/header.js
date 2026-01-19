49dc3d6b2831a596f45df51b57075fb6969cc8ba
Adam Griffith <adamgriffith@Adams-MacBook-Air.local>
2026-01-19 14:25:37 +0000

    Add Gleeds logo to header

--- js/components/header.js
+++ js/components/header.js
@@ -7,6 +7,9 @@ export function renderHeader(container, { lastUpdated, onSearch }) {
 
   container.innerHTML = `
     <div class="header-top">
+      <div class="header-brand">
+        <img src="https://images.seeklogo.com/logo-png/6/1/gleeds-logo-png_seeklogo-61171.png" alt="Gleeds" class="header-logo" />
+      </div>
       <h1 class="header-title">UK Infrastructure Dashboard</h1>
     </div>
     <p class="header-subtitle">Addressable Market Analysis by Region, Sector & Discipline</p>
